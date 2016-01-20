91a7dcfed0976d10991c4ebdba2349fb981809aa
Tim Sutton <tim@kartoza.com>
2016-01-20 20:56:56 +0200

    Improvements to build script and templates

--- build.sh
+++ build.sh
@@ -4,8 +4,13 @@ cd /inasafe
 git fetch
 git clean -df
 git reset --hard origin/develop
-scripts/release.sh 3.2.0
-mv /tmp/inasafe.3.2.0.zip /build
+VERSION=`cat metadata.txt | grep ^version | sed 's/version=//g'`
+scripts/release.sh
+mv /tmp/inasafe.${VERSION}.zip /build
 echo "This is the InaSAFE Nightly Build repo last build at : " > /build/index.html
 date >> /build/index.html
 touch /build/index.html
+cp /build/plugins.xml.tmpl /build/plugins.xml
+rpl "[[VERSION]]" "${VERSION}" /build/plugins.xml
+DATE=`date`
+rpl "[[DATE]]" "${DATE}" /build/plugins.xml
