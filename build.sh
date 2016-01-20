c3d73e112db31152e5b335d8929018364e9ad552
Tim Sutton <tim@kartoza.com>
2016-01-20 22:22:12 +0200

    Fixes for build script

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ git reset --hard origin/develop
 VERSION=`cat metadata.txt | grep ^version | sed 's/version=//g'`
 scripts/release.sh
 mv /tmp/inasafe.${VERSION}.zip /build
-mv /tmp/inasafe.${VERSION}.exe /build
+mv /tmp/inasafe.${VERSION}-plugin.exe /nbuild
 echo "This is the InaSAFE Nightly Build repo last build at : " > /build/index.html
 date >> /build/index.html
-echo "</br>Windows installer <a href='/inasafe.${VERSION}.exe'>inasafe.${VERSION}.exe</a></br>"
-echo "Plugin repo: http://nightly.inasafe.org/plugins.xml"
+echo "</br>Windows installer <a href='/inasafe.${VERSION}-plugin.exe'>inasafe.${VERSION}-plugin.exe</a></br>" >> /build/index.html
+echo "Plugin repo: http://nightly.inasafe.org/plugins.xml" >> /build/index.html
 touch /build/index.html
 cp /build/plugins.xml.tmpl /build/plugins.xml
 rpl "[[VERSION]]" "${VERSION}" /build/plugins.xml
