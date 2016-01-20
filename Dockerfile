f4fa180c466417790e6c0e39d2b0d62ebcaf25ac
Tim Sutton <tim@kartoza.com>
2016-01-20 21:17:26 +0200

    Also add urls and build win package

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM ubuntu:14.04
 MAINTAINER tim@kartoza.com
 
 RUN apt-get -y update
-RUN apt-get -y install git rpl zip
+RUN apt-get -y install git rpl zip make nsis
 RUN git clone git://github.com/AIFDR/inasafe.git
 
 ADD package-cron /etc/cron.d/package-cron
--- build.sh
+++ build.sh
@@ -7,8 +7,11 @@ git reset --hard origin/develop
 VERSION=`cat metadata.txt | grep ^version | sed 's/version=//g'`
 scripts/release.sh
 mv /tmp/inasafe.${VERSION}.zip /build
+mv /tmp/inasafe.${VERSION}.exe /build
 echo "This is the InaSAFE Nightly Build repo last build at : " > /build/index.html
 date >> /build/index.html
+echo "</br>Windows installer <a href='/inasafe.${VERSION}.exe'>inasafe.${VERSION}.exe</a></br>"
+echo "Plugin repo: http://nightly.inasafe.org/plugins.xml"
 touch /build/index.html
 cp /build/plugins.xml.tmpl /build/plugins.xml
 rpl "[[VERSION]]" "${VERSION}" /build/plugins.xml
