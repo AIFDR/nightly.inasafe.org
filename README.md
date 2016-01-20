572c9fa0c62a5aaf31a49a87969f6c34eb6bd55d
Tim Sutton <tim@kartoza.com>
2016-01-20 22:38:53 +0200

    Fixed exe copying

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Docker scripts for nightly builds at http://nightly.inasafe.org
 
 See https://github.com/AIFDR/inasafe/wiki/Nightly%20Builds
+
+
+To manually build:
+
+```
+docker-compose build
+docker-compose up -d
+docker exec -ti nightlyinasafeorg_cron_1 /bin/bash -c "/build.sh"
+```
+
--- build.sh
+++ build.sh
@@ -7,10 +7,10 @@ git reset --hard origin/develop
 VERSION=`cat metadata.txt | grep ^version | sed 's/version=//g'`
 scripts/release.sh
 mv /tmp/inasafe.${VERSION}.zip /build
-mv /tmp/inasafe.${VERSION}-plugin.exe /nbuild
+mv /tmp/InaSAFE-${VERSION}-plugin.exe /nbuild
 echo "This is the InaSAFE Nightly Build repo last build at : " > /build/index.html
 date >> /build/index.html
-echo "</br>Windows installer <a href='/inasafe.${VERSION}-plugin.exe'>inasafe.${VERSION}-plugin.exe</a></br>" >> /build/index.html
+echo "</br>Windows installer <a href='/InaSAFE-${VERSION}-plugin.exe'>InaSAFE-${VERSION}-plugin.exe</a></br>" >> /build/index.html
 echo "Plugin repo: http://nightly.inasafe.org/plugins.xml" >> /build/index.html
 touch /build/index.html
 cp /build/plugins.xml.tmpl /build/plugins.xml
