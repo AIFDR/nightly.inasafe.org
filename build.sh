5d0eb08ecceb55208d3b5baddea9b24039e86015
Tim Sutton <tim@kartoza.com>
2016-07-27 13:36:07 +0200

    Updated build process and added log file link to build

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 cd /inasafe
-git checkout develop
-git fetch
-git clean -df
-git reset --hard origin/develop
+git checkout develop > /tmp/nightly.log
+git fetch >> /tmp/nightly.log
+git clean -df >> /tmp/nightly.log
+git reset --hard origin/develop >> /tmp/nightly.log
 VERSION=`cat metadata.txt | grep ^version | sed 's/version=//g'`
-scripts/release.sh
-mv /tmp/nightly.log /build/log.txt
+scripts/release.sh >> /tmp/nightly.log
 mv /tmp/inasafe.${VERSION}.zip /build
 mv /tmp/InaSAFE-${VERSION}-plugin.exe /build
 cat /build/header.html > /build/index.html
@@ -16,9 +15,8 @@ date >> /build/index.html
 echo "</p><p class='text-center'>Windows installer <a href='/InaSAFE-${VERSION}-plugin.exe'>InaSAFE-${VERSION}-plugin.exe</a></br>" >> /build/index.html
 echo "</p><p class='text-center'>Plugin repo: http://nightly.inasafe.org/plugins.xml" >> /build/index.html
 echo "</p><p class='text-center'>Usage: <a href='https://github.com/AIFDR/inasafe/wiki/Nightly%20Builds'>instructions</a>" >> /build/index.html
-echo "</p><p class='text-center'>Build log: <a href='/log,txt'>"
+echo "</p><p class='text-center'>Build log: <a href='/log.txt'>View" >> /build/index.html
 echo "</a>" >> /build/index.html
-date >> /build/index.html
 echo "</p>" >> /build/index.html
 cat /build/footer.html >> /build/index.html
 touch /build/index.html
@@ -26,3 +24,4 @@ cp /build/plugins.xml.tmpl /build/plugins.xml
 rpl "[[VERSION]]" "${VERSION}" /build/plugins.xml
 DATE=`date`
 rpl "[[DATE]]" "${DATE}" /build/plugins.xml
+mv /tmp/nightly.log /build/log.txt
--- build_nightly_from_host.sh
+++ build_nightly_from_host.sh
@@ -2,4 +2,4 @@
 
 cd /home/data/nightly.inasafe.org
 # Don't use -i as we run this in cron which is non interactive
-docker exec -t nightlyinasafeorg_cron_1 /bin/bash -c "/build.sh"
+docker exec -t nightlyinasafeorg_cron_1 /bin/bash -c "/build.sh" 
