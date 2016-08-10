d0ef4c3262959ce9ec838cf7445b5f2aab1f4e71
Tim Sutton <tim@kartoza.com>
2016-08-10 16:12:22 +0200

    Fix container name used for experimental build

--- build_nightly_from_host.sh
+++ build_nightly_from_host.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/data/nightly.inasafe.org
+cd /home/data/experimental.inasafe.org
 # Don't use -i as we run this in cron which is non interactive
-docker exec -t nightlyinasafeorg_cron_1 /bin/bash -c "/build.sh" 
+docker exec -t experimentalinasafeorg_cron_1 /bin/bash -c "/build.sh" 
